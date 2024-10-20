bc92c6d5f94593fba4bb8cc1d1fd3ae35db4e740
evans kirimi <evanskirimi483@gmail.com>
2024-10-20 14:42:49 +0300

    modified

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -17,7 +17,7 @@ CREATE TABLE Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE Customer (
+CREATE TABLE Customers (
     customer_id INT AUTO INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL UNIQUE,
