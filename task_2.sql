d332d1bc5266d80c4d8794e45b1b7700b98fc793
evans kirimi <evanskirimi483@gmail.com>
2024-10-20 19:35:46 +0300

    modified

--- task_2.sql
+++ task_2.sql
@@ -39,11 +39,11 @@ CREATE TABLE IF NOT EXISTS Orders (
 
 -- Create order_details table
 CREATE TABLE IF NOT EXISTS Order_details (
-    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_detail INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT NOT NULL,
-    price DECIMAL(10, 2),
+    quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
-);
+)
+    
